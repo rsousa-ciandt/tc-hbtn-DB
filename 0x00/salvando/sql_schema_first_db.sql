b02ab9b011a5a1e5ca7ce040d862496ce875e9f1
rsousa <rsousa@ciandt.com>
2022-07-22 08:47:55 -0300

    feat: implementation of task "exportando schema com sqlite online"

---
+++ 0x00/salvando/sql_schema_first_db.sql
@@ -0,0 +1,15 @@
+-- TABLE
+CREATE TABLE CLIENTE (
+id integer primary key autoincrement not null,
+nome varchar(80) not null,
+email varchar(40) not null,
+telefone integer
+);
+CREATE TABLE sqlite_sequence(name,seq);
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
