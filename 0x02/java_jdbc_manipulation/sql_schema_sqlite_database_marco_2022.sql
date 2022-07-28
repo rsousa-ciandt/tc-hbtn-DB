9389e2251233bcc340c3de573baa91b9e46545fb
rsousa <rsousa@ciandt.com>
2022-07-28 09:25:10 -0300

    implementation of task "2 - Java com JDBC"

---
+++ 0x02/java_jdbc_manipulation/sql_schema_sqlite_database_marco_2022.sql
@@ -0,0 +1,10 @@
+-- TABLE
+CREATE TABLE CLIENTE (id integer PRIMARY KEY AUTOINCREMENT,nome varchar(100),idade integer,cpf varchar(15),rg varchar(15));
+CREATE TABLE sqlite_sequence(name,seq);
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
